a73db799f9098cf8418dd1f9a68f99913e7ed80a
Lucas-Mol <lucasmolpro@outlook.com>
2023-03-21 16:39:33 -0300

    [FEAT]: add Journal Exception Pattern to MailException

--- Journal/src/main/java/com/journal/exception/MailException.java
+++ Journal/src/main/java/com/journal/exception/MailException.java
@@ -3,5 +3,23 @@ package com.journal.exception;
 public class MailException extends Exception{
 
 	private static final long serialVersionUID = 1L;
+	
+	public String growlTitle;
+	
+	public String growlMessage;
+	
+	public MailException() {
+		
+	}
+	
+	public MailException( String growlMessage) {
+		this.growlTitle = "";
+		this.growlMessage = growlMessage;
+	}
+	
+	public MailException(String growlTitle, String growlMessage) {
+		this.growlTitle = growlTitle;
+		this.growlMessage = growlMessage;
+	}
 
 }
