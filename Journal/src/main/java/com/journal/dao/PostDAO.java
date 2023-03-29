4776774b165fdf24d87c93bd01728afe35953749
Lucas-Mol <lucasmolpro@outlook.com>
2023-03-28 21:06:49 -0300

    feat(PostDAO): create postDAO with basic CRUD methods TODO 'list post' and 'list post from label filter'

---
+++ Journal/src/main/java/com/journal/dao/PostDAO.java
@@ -0,0 +1,81 @@
+package com.journal.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import com.journal.model.Post;
+import com.journal.model.Post_;
+import com.journal.model.User;
+import com.journal.model.User_;
+
+public class PostDAO {
+
+	private EntityManager manager;
+	
+	private CriteriaBuilder builder;
+	private CriteriaQuery<Post> query;
+	private CriteriaQuery<Long> count;
+	private Root<Post> rootPost;
+	
+	public Post find(User user) {
+		manager = ConnectionFactory.getEntityManager();
+		builder = manager.getCriteriaBuilder();
+		query = builder.createQuery(Post.class);
+		rootPost = query.from(Post.class);
+		
+		query.where(builder.equal(rootPost.get(User_.ID), user.getId()));
+
+	    TypedQuery<Post> tq = manager.createQuery(query);
+	    Post result = tq.getSingleResult();
+	    manager.close();
+	    return result;
+	}
+	
+	public Post insert(Post post) {
+		manager = ConnectionFactory.getEntityManager();
+		EntityTransaction transaction = manager.getTransaction();
+				
+		transaction.begin();
+		manager.persist(post);
+		transaction.commit();
+		
+		manager.close();
+		return post;
+	}
+	
+	public Post edit(Post post) {
+		manager = ConnectionFactory.getEntityManager();
+		EntityTransaction transaction = manager.getTransaction();
+				
+		transaction.begin();
+		manager.merge(post);
+		transaction.commit();
+		
+		manager.close();
+		return post;
+	}
+	
+	public int remove(Post post) {
+		manager = ConnectionFactory.getEntityManager();
+		manager.getTransaction().begin();
+		builder = manager.getCriteriaBuilder();
+		CriteriaDelete<Post> deleteQuery = builder.createCriteriaDelete(Post.class);
+		rootPost = deleteQuery.from(Post.class);
+		
+		deleteQuery.where(builder.equal(rootPost.get(Post_.ID), post.getId()));
+		
+		Query query = manager.createQuery(deleteQuery);
+		
+		int affectedLines = query.executeUpdate();
+		manager.getTransaction().commit();
+		manager.close();
+
+		return affectedLines;
+	}
+}
