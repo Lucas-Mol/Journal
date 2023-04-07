b14f662c25f45497d3c408ce2ca9ccc009bd017e
Lucas-Mol <lucasmolpro@outlook.com>
2023-04-07 17:04:55 -0300

    feat(PostDataScrollerBB): create method to implement LazyDataModel to list Posts

--- Journal/src/main/java/com/journal/controller/backingBean/PostDataScrollerBB.java
+++ Journal/src/main/java/com/journal/controller/backingBean/PostDataScrollerBB.java
@@ -1,16 +1,20 @@
 package com.journal.controller.backingBean;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.primefaces.model.FilterMeta;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortMeta;
+
 import com.journal.enumeration.ColorEnum;
+import com.journal.model.Label;
+import com.journal.model.Post;
 import com.journal.model.User;
 import com.journal.service.PostService;
-import com.journal.util.GrowlUtils;
 
 @ManagedBean
 @ViewScoped
@@ -19,10 +23,53 @@ public class PostDataScrollerBB {
 	private PostService postService = new PostService();
 	
 	private User user;
+	private String labelFilter;
+	private LazyDataModel<Post> lsPosts;
 	
 	public PostDataScrollerBB (User user) {
 		this.user = user;
 
+		searchPosts(user, null);
+	}
+	
+	private void searchPosts(User user, Label label) {
+		
+		lsPosts = new LazyDataModel<Post>() {
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public List<Post> load(int first, int pageSize, Map<String, SortMeta> sortBy,
+					Map<String, FilterMeta> filterBy) {
+				
+				List<Post> result = postService.findPosts(user, label, first, pageSize);
+					
+				this.setRowCount(postService.countPosts(user, label).intValue());
+
+				return result;
+			}
+			
+			@Override
+			public int count(Map<String, FilterMeta> filterBy) {
+				// TODO Auto-generated method stub
+				return postService.countPosts(user, label).intValue();
+			}
+		};
+	}
+
+	public String getLabelFilter() {
+		return labelFilter;
+	}
+
+	public void setLabelFilter(String labelFilter) {
+		this.labelFilter = labelFilter;
+	}
+
+	public LazyDataModel<Post> getLsPosts() {
+		return lsPosts;
+	}
+
+	public void setLsPosts(LazyDataModel<Post> lsPosts) {
+		this.lsPosts = lsPosts;
 	}
 	
 
