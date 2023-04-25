7a936bc998bb650674b2348f9c4412183d6ec7dc
Lucas-Mol <lucasmolpro@outlook.com>
2023-04-25 18:39:14 -0300

    feat(): creating Label Service

---
+++ Journal/src/main/java/com/journal/service/LabelService.java
@@ -0,0 +1,38 @@
+package com.journal.service;
+
+import java.util.List;
+
+import com.journal.dao.LabelDAO;
+import com.journal.model.Label;
+import com.journal.model.Post;
+
+public class LabelService {
+
+	private LabelDAO labelDAO = new LabelDAO();
+	private PostService postService;
+	
+	public Label insertLabel(Label label) {
+		Label persistedLabel = verifyLabelAlreadyExist(label);
+		return (persistedLabel == null) ? labelDAO.insert(label) : persistedLabel;
+	}
+	
+	
+	public int removeIfNotUsed(Label label) {
+		int affectedLines = 0;
+		postService = new PostService();
+		
+		List<Post> postsSameLabel = postService.findByLabel(label);
+		
+		if(postsSameLabel != null && postsSameLabel.isEmpty())
+			affectedLines += labelDAO.remove(label);
+		
+		return affectedLines;
+	}
+	
+	
+	public Label verifyLabelAlreadyExist(Label label) {
+		Label persistedLabel = labelDAO.findByNameColor(label);
+		return persistedLabel;
+	}
+
+}
