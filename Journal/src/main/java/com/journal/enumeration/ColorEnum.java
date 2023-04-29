77398f31083f34d4310cb970919fabab866fa12c
Lucas-Mol <lucasmolpro@outlook.com>
2023-04-29 14:27:30 -0300

    feat(): jumping buttons 'send' and 'edit' on post editor lol

--- Journal/src/main/java/com/journal/enumeration/ColorEnum.java
+++ Journal/src/main/java/com/journal/enumeration/ColorEnum.java
@@ -2,10 +2,17 @@ package com.journal.enumeration;
 
 public enum ColorEnum {
 	
-	RED(0, "#A50F69"),
-	GREEN(1,"#444725"),
+	RED(0, "#DF2231"),
+	GREEN(1,"#839F18"),
 	BLUE(2, "#1E9AFF"),
-	YELLOW(3, "#FFE600");
+	YELLOW(3, "#FFD700"),
+	PINK(4, "#FF006F"),
+	PURPLE(5, "#873cbb"),
+	ORANGE(6, "#F97613"),
+	BROWN(7, "#654321"),
+	GOLD(8, "radial-gradient(ellipse farthest-corner at right bottom, #FEDB37 0%, #FDB931 8%, #9f7928 30%, #8A6E2F 40%, transparent 80%)," +
+						"radial-gradient(ellipse farthest-corner at left top, #FFFFFF 0%, #FFFFAC 8%, #D1B464 25%, #5d4a1f 62.5%, #5d4a1f 100%)"),
+	WHITE(9, "#FAFAFA");
 
 	private Integer id;
 	private String color;
